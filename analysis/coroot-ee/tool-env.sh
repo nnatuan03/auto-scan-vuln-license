ac689e148e3cbe0fbbfdcf6987d9cf5828890368
nnatuan03 <nnatuan2003@gmail.com>
2026-06-15 09:31:02 +0700

    Remove generated analysis outputs and tests

--- analysis/coroot-ee/tool-env.sh
+++
@@ -1,9 +0,0 @@
-# Source this file before running the reverse-engineering toolchain:
-#   source /Users/itsmac/Desktop/auto-scan-vuln-lic/analysis/coroot-ee/tool-env.sh
-
-export PATH="/Users/itsmac/go/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/opt/homebrew/opt/binutils/bin:$PATH"
-
-# Common tool locations:
-# - GoReSym, redress, govulncheck: /Users/itsmac/go/bin
-# - rizin/rz-bin/rz-asm, yara, osv-scanner, syft, grype, go, ghidraRun: /opt/homebrew/bin
-# - greadelf/gobjdump/gstrings: /opt/homebrew/opt/binutils/bin
